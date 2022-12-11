cb8f29d800483b63e3f1564bc221918cd8e56864
abdurrahman720 <personal.abdurrahman@gmail.com>
2022-12-12 02:13:15 +0600

    created blog pages

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -3,7 +3,26 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            This is Blog Page
+           <section className="dark:bg-gray-800 dark:text-gray-100">
+	<div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
+		<h2 className="text-2xl font-semibold sm:text-4xl">Some Important ReactJs Questions</h2>
+		<p className="mt-4 mb-8 dark:text-gray-400">We will be answering questions about some important ReactJs Topics</p>
+		<div className="space-y-4">
+			<details className="w-full border rounded-lg" open="">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-green-400">What is the purpose of React Router?</summary>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">Lectus iaculis orci metus vitae ligula dictum per. Nisl per nullam taciti at adipiscing est. </p>
+			</details>
+			<details className="w-full border rounded-lg" open="">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-green-400">How does Context API work in React?</summary>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">Tincidunt ut hac condimentum rhoncus phasellus nostra. Magna porttitor egestas tincidunt neque vehicula potenti. </p>
+			</details>
+			<details className="w-full border rounded-lg" open="">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-green-400">What is the use of <i>useHref</i> Hook in React? </summary>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">Justo libero tellus integer tincidunt justo semper consequat venenatis aliquet imperdiet. Ultricies urna proin fusce nulla pretium sodales vel magna et massa euismod vulputate sed. </p>
+			</details>
+		</div>
+	</div>
+</section>
         </div>
     );
 };
