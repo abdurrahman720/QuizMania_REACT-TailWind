7d850e3674fc41d55b9b7348435fb60e1277a6e0
abdurrahman720 <personal.abdurrahman@gmail.com>
2022-12-12 02:28:33 +0600

    completed blog pages

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -10,15 +10,17 @@ const Blog = () => {
 		<div className="space-y-4">
 			<details className="w-full border rounded-lg" open="">
 				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-green-400">What is the purpose of React Router?</summary>
-				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">Lectus iaculis orci metus vitae ligula dictum per. Nisl per nullam taciti at adipiscing est. </p>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">React Router lets user to navigate from components to components without refreshing the whole website while preserving user state. The main purpose of React Router is to make the whole website into a Single Page Application(SPA)</p>
 			</details>
 			<details className="w-full border rounded-lg" open="">
 				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-green-400">How does Context API work in React?</summary>
-				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">Tincidunt ut hac condimentum rhoncus phasellus nostra. Magna porttitor egestas tincidunt neque vehicula potenti. </p>
+                            <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">React Context API allows to share information to any React component, by storing it in a central place and allowing access to any component that requests it. By using <i>createContext</i> hook, the data is shared among the nested components. By using <i>useContext</i> hook, the data is accessed by the nested components.
+                                It makes it possible to pass data from parent to children nested deep down the component tree directly, instead of passing it down through a chain of props. </p>
 			</details>
 			<details className="w-full border rounded-lg" open="">
 				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-green-400">What is the use of <i>useHref</i> Hook in React? </summary>
-				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">Justo libero tellus integer tincidunt justo semper consequat venenatis aliquet imperdiet. Ultricies urna proin fusce nulla pretium sodales vel magna et massa euismod vulputate sed. </p>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
+The useRef Hook allows to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly. </p>
 			</details>
 		</div>
 	</div>
